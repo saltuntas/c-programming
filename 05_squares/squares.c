8ab478303cefddaa372717284eb0d811daa0c14a
Coursera Learner <nobody@nowhere.nul>
2020-02-04 17:07:01 +0000

    new program

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -7,8 +7,16 @@
  * offset (INCLUSIVE) and offset + size (EXCLUSIVE)
  */
 int isInRange(int coord, int offset, int size) {
+  if (coord>=offset && coord<size)
+    { return 1;
+	}
   // if coord is in range, return 1
-  // else, return 0
+  // else, return0
+
+  else
+    {
+      return 0;
+  }
   return 0;
 }
 
@@ -18,36 +26,89 @@ int isInRange(int coord, int offset, int size) {
  */
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
+  if(coord==offset || coord==(offset+size))
+    {
+      return 1;
+    }
+  else {
+    return 0;
+  }
   // return 1, else return 0
   return 0;
 }
 
+
 void squares(int size1, int x_offset, int y_offset, int size2) {
-  //compute the max of size1 and (x_offset + size2).  Call this w
+//compute the max of size1 and (x_offset + size2).  Call this w
+  int w;
+  if (size1>(x_offset+size2)
+      ) {
+      int w=size1;
+    }
+    else {
+      int w=x_offset+size2;
+    }
+    
 
+  
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h;
+  
+  if(size1>(y_offset+size2))
+      {
+	int h=size1;
+      }
+      else
+	{
+	  int h=y_offset+size2;
+	}
+	       
+  
   //count from 0 to h. Call the number you count with y
-
+  for (int y=0;y<h; y++)
+		 {
+		   
     //count from 0 to w. Call the number you count with x
-
+		   for (int x=0;x<w; x++)
+		     {
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
+		       if ((isInRange(x,x_offset,(x_offset+size2))==1 && isAtBorder(y,y_offset,(y_offset+size2-1))==1) || (isInRange(y,y_offset,(y_offset+size2))==1 && isAtBorder(x,x_offset,(x_offset+size2-1))==1))
+
+			 {
       //  OR
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-
+			   printf("*");
+			 }
+			   
       //if not,
       // check if EITHER
+		       
+		       else if((((x<size1) && (y==0)) || (y==(size1-1))) || ((y<size1) && (((x==0)) || x== (size1-1))))
+			 
+
+			 {
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-
+			   printf("#");
+			 }
       //else print a space
+		       else {
+			 printf(" ");
+		       }
+		     }
+		   printf("\n");
+		 }
+		
     //when you finish counting x from 0 to w, 
     //print a newline
 
+  
 }
+
+    
